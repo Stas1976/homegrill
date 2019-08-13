893cb1753a8c820be649c616db91e334a87741e2
Stanislavas Goldyckis <stanislavas.goldyckis@yahoo.com>
2019-08-13 16:18:10 +0300

    Updates

---
+++ precache-manifest.d01efc04332402c571214eca9cd972ac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d2d6551805b05248185f2ddd28bc4a",
+    "url": "Stas1976.github.io/homegrill/index.html"
+  },
+  {
+    "revision": "7da4f379f26081f4600e",
+    "url": "Stas1976.github.io/homegrill/static/css/main.bad865e1.chunk.css"
+  },
+  {
+    "revision": "06851c6e71b01ea8d2b1",
+    "url": "Stas1976.github.io/homegrill/static/js/2.3a1e131d.chunk.js"
+  },
+  {
+    "revision": "7da4f379f26081f4600e",
+    "url": "Stas1976.github.io/homegrill/static/js/main.75965eae.chunk.js"
+  },
+  {
+    "revision": "d1694d166364235304ec",
+    "url": "Stas1976.github.io/homegrill/static/js/runtime~main.e99ab132.js"
+  },
+  {
+    "revision": "12d7c29cef70a1e19b876f20e03b7b16",
+    "url": "Stas1976.github.io/homegrill/static/media/cake.12d7c29c.png"
+  },
+  {
+    "revision": "d5055ab8985a1dee48a48f8d0f96bf93",
+    "url": "Stas1976.github.io/homegrill/static/media/drinks.d5055ab8.png"
+  },
+  {
+    "revision": "ad7538d118c2c5ab5bbbd4e0fa9a52b2",
+    "url": "Stas1976.github.io/homegrill/static/media/main_dish.ad7538d1.png"
+  },
+  {
+    "revision": "f51252718a4dada7f24907ed0a3a97be",
+    "url": "Stas1976.github.io/homegrill/static/media/special.f5125271.png"
+  }
+]);
